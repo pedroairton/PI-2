7252113ae4e823d503d607d9b3a71ae3c1cdb3b4
Pedro Airton <111513226+pedroairton@users.noreply.github.com>
2023-04-04 00:26:04 -0300

    Update script.js

--- app/script.js
+++ app/script.js
@@ -1,73 +1,100 @@
-// function mediaUm() {
-//     // Obter os valores das respostas
-//     let response1 = Number(document.getElementById('response1').value);
-//     let response2 = Number(document.getElementById('response2').value);
-//     let response3 = Number(document.getElementById('response3').value);
-  
-//     // Calcular a média
-//     let media = (response1 + response2 + response3) / 3;
-  
-//     // Exibir a média
-//     alert('A média das respostas é: ' + media);
-//   }
+let qt = []
 
-// let pgt = document.querySelectorAll('#star')
+qt[0] = document.querySelectorAll('.star.p1')
 
-
-let nota1
-let nota2
-let nota3
-let nota4
-let nota5
-
-function ativaV() {
-    let opt = document.querySelector('#star5')
-    opt.classList.toggle('ativo')
-    nota1 = 5
-    console.log(nota1)
-
-}
-function ativaIV() {
-    let opt = document.querySelector('#star4')
-    opt.classList.toggle('ativo')
-    nota1 = 4
-    console.log(nota1)
-
-}
-function ativaIII() {
-    let opt = document.querySelector('#star3')
-    opt.classList.toggle('ativo')
-    nota1 = 3
-    console.log(nota1)
-
-}
-function ativaII() {
-    let opt = document.querySelector('#star2')
-    opt.classList.toggle('ativo')
-    nota1 = 2
-    console.log(nota1)
-
-}
-function ativaI() {
-    let opt = document.querySelector('#star1')
-    opt.classList.toggle('ativo')
-    nota1 = 1
-    console.log(nota1)
-}
-
-let i = 1
-let pergunta = []
-
-while(i<6) {
-    
-    pergunta[i] = document.querySelector(`#star${i}`)
-    pergunta[i].addEventListener('click',function() {
-        let nota = pergunta.value
-        console.log(nota)
+qt[0].forEach(function(element) {
+    element.addEventListener('click',function() {
+        console.log(element.value)
+        // element.classList.add('ativo')
+        qt[0] = Number(element.value)
     })
-    console.log(pergunta[i].value)
-    i++
+})
 
+// PERGUNTA 2 
+
+qt[1] = document.querySelectorAll('.star.p2')
+
+qt[1].forEach(function(element) {
+    element.addEventListener('click',function() {
+        console.log(element.value)
+        // element.classList.add('ativo')
+        qt[1] = Number(element.value)
+    })
+})
+
+// PERGUNTA 3
+
+qt[2] = document.querySelectorAll('.star.p3')
+
+qt[2].forEach(function(element) {
+    element.addEventListener('click',function() {
+        console.log(element.value)
+        // element.classList.add('ativo')
+        qt[2] = Number(element.value)
+    })
+})
+
+// PERGUNTA 4
+
+qt[3] = document.querySelectorAll('.star.p4')
+
+qt[3].forEach(function(element) {
+    element.addEventListener('click',function() {
+        console.log(element.value)
+        // element.classList.add('ativo')
+        qt[3] = Number(element.value)
+    })
+})
+
+// PERGUNTA 5
+
+qt[4] = document.querySelectorAll('.star.p5')
+
+qt[4].forEach(function(element) {
+    element.addEventListener('click',function() {
+        console.log(element.value)
+        // element.classList.add('ativo')
+        qt[4] = Number(element.value)
+    })
+})
+
+const rst1 = document.querySelector('.result.f1')
+
+
+function calculo() {
+    let media = (qt[0]+qt[1]+qt[2]+qt[3]+qt[4]) / qt.length
+    console.log(media)
+    rst1.innerHTML = (`A média é ${media}`)
 }
 
-  
+// ESTILIZAÇÃO
+
+
+
+
+let p1 = document.querySelectorAll('.rating .star.p1')
+
+let label = document.querySelectorAll('.rating .star.p1')
+
+label.forEach(function(element,index,array) {
+  element.addEventListener('click',function() {
+    // console.log(array)
+    let i = 0
+    while(i <= array.length - 1) {
+      array[i].classList.remove('ativo')
+      i++
+    }
+    array[index].classList.add('ativo')
+  })
+})
+
+// estilizar classe ativo!
+
+
+
+
+
+
+
+
+
