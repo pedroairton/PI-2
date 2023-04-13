ac89b10871855463078e32a816ab4fd2148f8cb6
Pedro Airton <111513226+pedroairton@users.noreply.github.com>
2023-04-13 20:01:43 -0300

    Update script.js

--- app/script.js
+++ app/script.js
@@ -69,106 +69,45 @@ function calculo() {
 
 // ESTILIZAÇÃO
 
-let i = 0
-let label = document.querySelectorAll(`.rating .label.p${i+1}`)
 
-//while(i = 0 < label.length) {
-//    label[i] = document.querySelector(`.rating .label.p${i+1}`)
-//i++
-}
-label.forEach(function(element,index,array) {
-    element.addEventListener('click',function() {
-      console.log(array)
-      let i = 0
-      while(i <= array.length - 1) {
-        array[i].classList.remove('ativo')
-        i++
-      }
-      for (let ix = 0; ix <= index; ix++) {
-          array[ix].classList.add('ativo')
-      }
-    })
+let label = []
+
+label[0] = document.querySelectorAll(`.rating .label.p1`)
+
+label[1] = document.querySelectorAll('.rating .label.p2')
+
+label[2] = document.querySelectorAll('.rating .label.p3')
+
+label[3] = document.querySelectorAll('.rating .label.p4')
+
+label[4] = document.querySelectorAll('.rating .label.p5')
+
+label[5] = document.querySelectorAll('.rating .label.p6')
+
+label[6] = document.querySelectorAll('.rating .label.p7')
+
+label[7] = document.querySelectorAll('.rating .label.p8')
+
+label[8] = document.querySelectorAll('.rating .label.p9')
+
+label[9] = document.querySelectorAll('.rating .label.p10')
+
+
+label.forEach(function(emt,idx,ary) {
+  label[idx].forEach(function(element,index,array) {
+  element.addEventListener('click',function() {
+    console.log(array)
+    let i = 0
+    while(i <= array.length - 1) {
+      array[i].classList.remove('ativo')
+      i++
+    }
+    for (let ix = 0; ix <= index; ix++) {
+        array[ix].classList.add('ativo')
+    }
   })
-
-// let label1 = document.querySelectorAll('.rating .label.p1')
-
-// label1.forEach(function(element,index,array) {
-//   element.addEventListener('click',function() {
-//     console.log(array)
-//     let i = 0
-//     while(i <= array.length - 1) {
-//       array[i].classList.remove('ativo')
-//       i++
-//     }
-//     for (let ix = 0; ix <= index; ix++) {
-//         array[ix].classList.add('ativo')
-//     }
-//   })
-// })
-
-// let label2 = document.querySelectorAll('.rating .label.p2')
-
-// label2.forEach(function(element,index,array) {
-//   element.addEventListener('click',function() {
-//     console.log(array)
-//     let i = 0
-//     while(i <= array.length - 1) {
-//       array[i].classList.remove('ativo')
-//       i++
-//     }
-//     for (let ix = 0; ix <= index; ix++) {
-//         array[ix].classList.add('ativo')
-//     }
-//   })
-// })
-
-// let label3 = document.querySelectorAll('.rating .label.p3')
-
-// label3.forEach(function(element,index,array) {
-//   element.addEventListener('click',function() {
-//     console.log(array)
-//     let i = 0
-//     while(i <= array.length - 1) {
-//       array[i].classList.remove('ativo')
-//       i++
-//     }
-//     for (let ix = 0; ix <= index; ix++) {
-//         array[ix].classList.add('ativo')
-//     }
-//   })
-// })
-
-// let label4 = document.querySelectorAll('.rating .label.p4')
-
-// label4.forEach(function(element,index,array) {
-//   element.addEventListener('click',function() {
-//     console.log(array)
-//     let i = 0
-//     while(i <= array.length - 1) {
-//       array[i].classList.remove('ativo')
-//       i++
-//     }
-//     for (let ix = 0; ix <= index; ix++) {
-//         array[ix].classList.add('ativo')
-//     }
-//   })
-// })
-
-// let label5 = document.querySelectorAll('.rating .label.p5')
-
-// label5.forEach(function(element,index,array) {
-//   element.addEventListener('click',function() {
-//     console.log(array)
-//     let i = 0
-//     while(i <= array.length - 1) {
-//       array[i].classList.remove('ativo')
-//       i++
-//     }
-//     for (let ix = 0; ix <= index; ix++) {
-//         array[ix].classList.add('ativo')
-//     }
-//   })
-// })
+})
+})
 
 
 
@@ -183,7 +122,6 @@ label.forEach(function(element,index,array) {
 //     array[index].classList.add('ativo')
 //   })
 // })
-
 
 
 
